b78af4ae9a46fb897b0b42abf54e8946099e0dfb
fitz35 <clement@lahoche.info>
2024-06-19 10:21:14 +0200

    add some screen and power consumption

--- polybar/bar_control/toggle_informations.sh
+++ polybar/bar_control/toggle_informations.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+polybar-msg action "#power_consumption.module_toggle"
 polybar-msg action "#memory.module_toggle"
 polybar-msg action "#cpu.module_toggle"
 polybar-msg action "#temperature.module_toggle"
